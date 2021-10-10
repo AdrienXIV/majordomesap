59ecf882ca62cb9585c02b93faadc0789264fa06
Adrien M <adrien.mld@laposte.net>
2021-10-10 14:18:21 +0200

    📦 NEW: primereact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import "primereact/resources/themes/saga-blue/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
+import axios from "axios"
+
+axios.defaults.baseURL = process.env.URL // http://localhost:3000
+axios.defaults.headers = {
+  "Content-Type": "application/json",
+  credentials: "same-origin",
+  mode: "same-origin",
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
