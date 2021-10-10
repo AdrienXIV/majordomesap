0eac312ea11a672051521b253dc6154ad47f346b
Adrien M <adrien.mld@laposte.net>
2021-10-10 15:27:09 +0200

    📦 NEW: initialisation titre pro reconnu rncp

---
+++ components/accueil/TitreProReconnuRncp.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next"
+
+const TitreProReconnuRncp: NextPage = () => {
+  return <section id="titre-pro-reconnu-rncp"></section>
+}
+
+export default TitreProReconnuRncp
