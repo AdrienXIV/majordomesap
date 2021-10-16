446f6a0648335d8416c965fb3f561d2a31cddcc0
Adrien M <adrien.mld@laposte.net>
2021-10-16 13:16:54 +0200

    📦 NEW: section une équipe qualifiée

---
+++ components/accueil/UneEquipeQualifiee.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const UneEquipeQualifiee: NextPage = () => {
+  return (
+    <section id="une-equipe-qualifiee">
+      <div>
+        <div className="bloc-image">
+          <Image src="/images/equipe.jpg" layout="fill" objectFit="cover" />
+        </div>
+        <div className="bloc-texte">
+          <h2>Une équipe Qualifiée et Pédagogue</h2>
+          <p>
+            Depuis 2017, nous nous engageons à fournir un savoir-faire et un savoir-être nécessaires pour le déploiement et l’épanouissement
+            professionnel de chacun de nos apprenants. Afin de vous offrir une qualité optimale, Majordome Formation SAP a composé son équipe de
+            professionnels hautement qualifiés possédant plus de 20 ans d’expertise sur le terrain.
+          </p>
+          <p>
+            Nous proposons des modules de formation certifiants permettant de valoriser vos compétences métier et ainsi d’
+            <b>obtenir un titre professionnel reconnu de niveau 3 inscrit au RNCP.</b>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UneEquipeQualifiee;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import React from "react"
-import type { NextPage } from "next"
-import Footer from "@components/other/Footer"
-import Header from "@components/other/Header"
-import Meta from "@components/other/Meta"
-import TitreProReconnuRncp from "@components/accueil/TitreProReconnuRncp"
-import Head from "next/head"
-import ImageAccueil from "@components/image-accueil/ImageAccueil"
-import Soyez100Finance from "@components/accueil/Soyez100Finance"
-import Rncp from "@components/rncp/Rncp"
-import NosFormationsPersonnalisables from "@components/accueil/NosFormationsPersonnalisables"
-import PersonnalisezVosModules from "@components/accueil/PersonnalisezVosModules"
+import React from "react";
+import type { NextPage } from "next";
+import Footer from "@components/other/Footer";
+import Header from "@components/other/Header";
+import Meta from "@components/other/Meta";
+import TitreProReconnuRncp from "@components/accueil/TitreProReconnuRncp";
+import Head from "next/head";
+import ImageAccueil from "@components/image-accueil/ImageAccueil";
+import Soyez100Finance from "@components/accueil/Soyez100Finance";
+import Rncp from "@components/rncp/Rncp";
+import NosFormationsPersonnalisables from "@components/accueil/NosFormationsPersonnalisables";
+import PersonnalisezVosModules from "@components/accueil/PersonnalisezVosModules";
+import UneEquipeQualifiee from "@components/accueil/UneEquipeQualifiee";
 
 const Home: NextPage = () => {
   return (
@@ -29,10 +30,11 @@ const Home: NextPage = () => {
         <Soyez100Finance />
         <NosFormationsPersonnalisables />
         <PersonnalisezVosModules />
+        <UneEquipeQualifiee />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
