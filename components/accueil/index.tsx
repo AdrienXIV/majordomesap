a99b4fcae9289fd596afca3b8b5884c05b2d87c5
Adrien M <adrien.mld@laposte.net>
2021-10-18 18:55:43 +0200

    👌 IMPROVE: restructuration

---
+++ components/accueil/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import IlsNousOntFaitConfiances from "./IlsNousOntFaitConfiances";
+import ImageAccueil from "./ImageAccueil";
+import PersonnalisezVosModules from "./PersonnalisezVosModules";
+import Rncp from "./Rncp";
+import Soyez100Finance from "./Soyez100Finance";
+import TitreProReconnuRncp from "./TitreProReconnuRncp";
+import UneDemarcheSimple from "./UneDemarcheSimple";
+import UneEquipeQualifiee from "./UneEquipeQualifiee";
+
+const Accueil = () => {
+  return (
+    <>
+      <ImageAccueil />
+      <TitreProReconnuRncp />
+      <Soyez100Finance />
+      <Rncp />
+      <PersonnalisezVosModules />
+      <UneEquipeQualifiee />
+      <UneDemarcheSimple />
+      <IlsNousOntFaitConfiances />
+    </>
+  );
+};
+
+export default Accueil;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,8 @@ import React from "react";
 import type { NextPage } from "next";
 import Footer from "@components/other/Footer";
 import Header from "@components/other/Header";
-import Meta from "@components/other/Meta";
-import TitreProReconnuRncp from "@components/accueil/TitreProReconnuRncp";
 import Head from "next/head";
-import ImageAccueil from "@components/image-accueil/ImageAccueil";
-import Soyez100Finance from "@components/accueil/Soyez100Finance";
-import Rncp from "@components/rncp/Rncp";
-import NosFormationsPersonnalisables from "@components/accueil/NosFormationsPersonnalisables";
-import PersonnalisezVosModules from "@components/accueil/PersonnalisezVosModules";
-import UneEquipeQualifiee from "@components/accueil/UneEquipeQualifiee";
-import UneDemarcheSimple from "@components/accueil/UneDemarcheSimple";
-import IlsNousOntFaitConfiances from "@components/accueil/IlsNousOntFaitConfiances";
+import Accueil from "@components/accueil";
 
 const Home: NextPage = () => {
   return (
@@ -23,14 +14,7 @@ const Home: NextPage = () => {
       <Header />
 
       <main>
-        <ImageAccueil />
-        <TitreProReconnuRncp />
-        <Soyez100Finance />
-        <Rncp />
-        <PersonnalisezVosModules />
-        <UneEquipeQualifiee />
-        <UneDemarcheSimple />
-        <IlsNousOntFaitConfiances />
+        <Accueil />
       </main>
       <Footer />
     </>
