0e5e3f936b9fdd2df901c5d0dd1d14dd008374d9
Adrien M <adrien.mld@laposte.net>
2021-10-18 20:39:06 +0200

    📦 NEW: page formations (manque le lien pour la formation advf)

--- pages/formations.tsx
+++ pages/formations.tsx
@@ -3,7 +3,8 @@ import type { NextPage } from "next";
 import Footer from "@components/other/Footer";
 import Header from "@components/other/Header";
 import Head from "next/head";
-import Formations from "@components/formations";
+import Image from "next/image";
+import Link from "next/link";
 
 const FormationsPage: NextPage = () => {
   return (
@@ -14,7 +15,28 @@ const FormationsPage: NextPage = () => {
       <Header />
 
       <main>
-        <Formations />
+        <section id="formations">
+          <h2>Formations</h2>
+          <div className="p-grid">
+            <Link href="/formation-majordomat" passHref>
+              <a className="bloc-image">
+                <Image src="/images/AdobeStock_215615394-scaled.jpeg" layout="fill" objectFit="cover" quality={80} />
+                <div>
+                  <h3>Formation MAJORDOMAT</h3>
+                </div>
+              </a>
+            </Link>
+
+            <Link href="/formation-majordomat" passHref>
+              <a className="bloc-image">
+                <Image src="/images/AdobeStock_236366402-1.jpeg" layout="fill" objectFit="cover" quality={80} />
+                <div>
+                  <h3>Formation ADVF</h3>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </section>
       </main>
       <Footer />
     </>
