6c8c20f564426fe86e1b763f7a3c66dfc0fd3976
Adrien M <adrien.mld@laposte.net>
2021-10-24 13:45:24 +0200

    📦 NEW: section des sponsors de la aprtie financement

--- components/financement/index.tsx
+++ components/financement/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NhesitezPLus from "./NhesitezPlus";
+import Sponsors from "./Sponsors";
 
 const Financement = () => {
   return (
     <>
       <NhesitezPLus />
+      <Sponsors />
     </>
   );
 };
