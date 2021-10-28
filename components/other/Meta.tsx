70443aee85bd91443f6504a91af4026157da94cd
Adrien M <adrien.mld@laposte.net>
2021-10-28 18:44:25 +0200

    🐛 FIX:

--- components/other/Meta.tsx
+++ components/other/Meta.tsx
@@ -6,42 +6,45 @@ const Meta: NextPage = () => {
   return (
     <Head>
       <title>Sap Majordome</title>
-      <meta name="Content-Type" content="UTF-8"/>
-      <meta name="Content-Language" content="fr"/>
-      <meta name="Description" content="Formation majordome et d'assistance de vie"/>
-      <meta name="Keywords" content="Formation, Assitance, Majordome, Vie"/>
-      <meta name="Subject" content="Association de formation"/>
-      <meta name="Copyright" content="Dev Agency"/>
-      <meta name="Author" content="Allan BUSI, Adrien Malliard, Sarah Ouriachi"/>
-      <meta name="Publisher" content="Allan BUSI"/>
-      <meta name="Identifier-Url" content="https://majordomeformationsap.fr/"/>
-      <meta name="Revisit-After" content="31 days"/>
-      <meta name="Robots" content="index"/>
-      <meta name="Rating" content="general"/>
-      <meta name="Distribution" content="global"/>
-      <meta name="Expires" content="12/12/2025"/>
-      <meta name="Geography" content="Issy les moulinaux"/>
-      <meta name="Category" content="economics"/>
-      <meta name="DC.Title" content="Sap Majordome"/>
-      <meta name="DC.Content-Type" content="UTF-8"/>
-      <meta name="DC.Content-Language" content="fr"/>
-      <meta name="DC.Description" content="Formation majordome et d'assistance de vie"/>
-      <meta name="DC.Keywords" content="Formation, Assitance, Majordome, Vie"/>
-      <meta name="DC.Subject" content="Association de formation"/>
-      <meta name="DC.Copyright" content="Dev Agency"/>
-      <meta name="DC.Author" content="Allan BUSI, Adrien Malliard, Sarah Ouriachi"/>
-      <meta name="DC.Publisher" content="Allan BUSI"/>
-      <meta name="DC.Identifier-Url" content="https://majordomeformationsap.fr/"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://metatags.io/"/>
-      <meta property="og:title" content="Sap Formation"/>
-      <meta property="og:description" content="A remplir"/>
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://metatags.io/"/>
-      <meta property="twitter:title" content="Sap Formation"/>
-      <meta property="twitter:description" content="A remplir"/>
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      <meta name="Content-Type" content="UTF-8" />
+      <meta name="Content-Language" content="fr" />
+      <meta name="Description" content="Formation majordome et d'assistance de vie" />
+      <meta name="Keywords" content="Formation, Assitance, Majordome, Vie" />
+      <meta name="Subject" content="Association de formation" />
+      <meta name="Copyright" content="Dev Agency" />
+      <meta name="Author" content="Allan BUSI, Adrien Maillard, Sarah Ouriachi" />
+      <meta name="Publisher" content="Allan BUSI" />
+      <meta name="Identifier-Url" content="https://majordomeformationsap.fr/" />
+      <meta name="Revisit-After" content="31 days" />
+      <meta name="Robots" content="index" />
+      <meta name="Rating" content="general" />
+      <meta name="Distribution" content="global" />
+      <meta name="Expires" content="12/12/2025" />
+      <meta name="Geography" content="Issy les moulinaux" />
+      <meta name="Category" content="economics" />
+      <meta name="DC.Title" content="Sap Majordome" />
+      <meta name="DC.Content-Type" content="UTF-8" />
+      <meta name="DC.Content-Language" content="fr" />
+      <meta name="DC.Description" content="Formation majordome et d'assistance de vie" />
+      <meta name="DC.Keywords" content="Formation, Assitance, Majordome, Vie" />
+      <meta name="DC.Subject" content="Association de formation" />
+      <meta name="DC.Copyright" content="Dev Agency" />
+      <meta name="DC.Author" content="Allan BUSI, Adrien Malliard, Sarah Ouriachi" />
+      <meta name="DC.Publisher" content="Allan BUSI" />
+      <meta name="DC.Identifier-Url" content="https://majordomeformationsap.fr/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Sap Formation" />
+      <meta property="og:description" content="A remplir" />
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Sap Formation" />
+      <meta property="twitter:description" content="A remplir" />
+      <meta
+        property="twitter:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
