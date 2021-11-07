c35ed5bb3ad61fdff2da1d1b43d15d541f2b0348
Adrien M <adrien.mld@laposte.net>
2021-11-07 16:12:36 +0100

    👌 IMPROVE: images d'accueils + responsive

--- components/accueil/ImageAccueil.tsx
+++ components/accueil/ImageAccueil.tsx
@@ -7,12 +7,14 @@ import { myLoader } from "@utils/loader";
 
 const ImageAccueil: NextPage = () => {
   const [image, setImage] = useState("/images/1.jpg");
+  const [className, setClassName] = useState("shadow-black");
 
   useEffect(() => {
-    let url = "";
     let random = Math.floor(Math.random() * 5);
     // 0 n'existe pas dans les images
     if (random === 0) random = 1;
+
+    if (random === 1) setClassName("shadow-white");
 
     // maj de l'image au hasard
     setImage(`/images/${random}.jpg`);
@@ -24,8 +26,11 @@ const ImageAccueil: NextPage = () => {
         <Image loader={myLoader} alt="image aléatoire" src={image} quality={80} layout="fill" objectFit="cover" />
       </Suspense>
       <div className="hero-content-area">
-        <h1>Devenez Majordome</h1>
-        <h3>Titre Reconnue par l'état et le ministère du travail</h3>
+        <h1 className={className}>
+          Devenez Majordome <br />
+          Titre Reconnue par l'état et le ministère du travail
+        </h1>
+
         <Link href="/#contact" passHref>
           <a className="btn">Nous rejoindre</a>
         </Link>
