789d6c04a7ce4196029b68b38bd9c1c8515de090
AllanBUSI <busi.travail@gmail.com>
2021-12-06 08:00:36 +0100

    build faile

--- pages/formation-majordomat.tsx
+++ pages/formation-majordomat.tsx
@@ -15,8 +15,8 @@ const FormationMajordomatPage: NextPage = () => {
       <Head>
         <title>Formation Majordomat | Majordome Formation SAP</title>
         <MetaSite>
-          <meta name="description" content="Formez-vous aux métiers du majordomat, en rejoignant notre équipe qualifiée et pédagogue avec des modules de qualité. Formation certifiante et financée, vous prépare à vous démarquer sur le marché de l’emploi.">
-          <meta name="keywords" content="formation gratuite, formation zéro à charge, formation certifiante, formation à distance, formation en ligne, formation pôle emploi, formation financée">
+          <meta name="description" content="Formez-vous aux métiers du majordomat, en rejoignant notre équipe qualifiée et pédagogue avec des modules de qualité. Formation certifiante et financée, vous prépare à vous démarquer sur le marché de l’emploi."/>
+          <meta name="keywords" content="formation gratuite, formation zéro à charge, formation certifiante, formation à distance, formation en ligne, formation pôle emploi, formation financée"/>
         </MetaSite>
       </Head>
       <Header />
