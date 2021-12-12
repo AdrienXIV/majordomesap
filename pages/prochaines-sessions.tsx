f39c431bf75ddb538c7414c668fbd421837c6254
Adrien M <adrien.mld@laposte.net>
2021-12-12 13:50:16 +0100

    👌 IMPROVE: textes prochaines sessions

--- pages/prochaines-sessions.tsx
+++ pages/prochaines-sessions.tsx
@@ -14,6 +14,11 @@ const ProchainesSessionsPage: NextPage = () => {
 
       <main>
         <DatesSessions />
+        <section id="prochaines-sessions">
+          <h3>Information collective du vendredi</h3>
+          <hr />
+          <p>Chaque vendredi à 11H, nous vous accueillons dans nos locaux ou en visio pour répondre à vos questions et nous présenter.</p>
+        </section>
       </main>
     </>
   );
