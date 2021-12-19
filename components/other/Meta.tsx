99eba050240ce1c7d4e605d9666fb4d6c3aa5cc2
Adrien M <adrien.mld@laposte.net>
2021-12-19 13:38:09 +0100

    👌 IMPROVE: lang

--- components/other/Meta.tsx
+++ components/other/Meta.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { NextPage } from "next";
 import Head from "next/head";
 
-const MetaSite = ({children}) => {
+const MetaSite = ({ children }) => {
   return (
-    <>
+    <Head>
       <meta name="Content-Type" content="UTF-8" />
       <meta name="Content-Language" content="fr" />
       <meta name="Subject" content="Association de formation" />
@@ -34,14 +33,14 @@ const MetaSite = ({children}) => {
       <meta
         property="twitter:image"
         content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+      />
       {children}
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
       <script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    </>
+    </Head>
   );
 };
 
