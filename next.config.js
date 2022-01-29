276f1e1da1e36627f12550f8cfccaba36c9925d9
Adrien M <adrien.mld@laposte.net>
2022-01-29 14:16:52 +0100

    📦 NEW: test cache controle imgs

--- next.config.js
+++ next.config.js
@@ -82,6 +82,16 @@ module.exports = {
           },
         ],
       },
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
     ];
   },
 };
