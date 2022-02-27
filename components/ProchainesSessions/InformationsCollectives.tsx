757d5cd6d7cd1e9a76707a0f147a0cef5f3944f3
Adrien M <adrien.mld@laposte.net>
2022-02-27 13:33:14 +0100

    📦 NEW: dates avril

--- components/ProchainesSessions/InformationsCollectives.tsx
+++ components/ProchainesSessions/InformationsCollectives.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { myLoader } from "@utils/loader";
+import { ProgressSpinner } from "primereact/progressspinner";
+import React, { Suspense } from "react";
+import Image from "next/image";
 
 const InformationsCollectives = () => {
   return (
@@ -6,6 +9,19 @@ const InformationsCollectives = () => {
       <h2>Information collective du vendredi</h2>
       <hr />
       <p>Chaque vendredi à 11H, nous vous accueillons dans nos locaux ou en visio pour répondre à vos questions et nous présenter.</p>
+
+      <Suspense fallback={<ProgressSpinner />}>
+        <br />
+        <br />
+        <Image
+          loader={myLoader}
+          alt="calendrier avril 2022"
+          src="/images/calendrier/sap_calendrier_avril_2022.png"
+          quality={80}
+          height={629}
+          width={919}
+        />
+      </Suspense>
     </section>
   );
 };
