5c555f5fd8785c559fe2b5a11165fe708ea6e434
AllanBUSI <busi.travail@gmail.com>
2022-03-03 09:46:57 +0100

    update$

--- components/ProchainesSessions/DatesSessions.tsx
+++ components/ProchainesSessions/DatesSessions.tsx
@@ -7,35 +7,55 @@ const DatesSessions = () => {
   return (
     <section id="dates-sessions">
       <h3>Dates de nos prochaines sessions</h3>
+      <p>La formation majordomat, vous permet d'obtenir les bases et la polyvalence d'un majordome. Lors de cette formation, vous acquerrez une vision globale du métier de majordome et des compétences indispensables pour y exercer dans les métiers du luxe. Vous apprendrez les savoir-être, le rôle du majordome et ses relations avec le personnel.</p>
+      <p>La formation d'Assistant(e) de Vie aux Familles s'adresse à toute personne souhaitant se former pour exercer ce métier. En effet, étant donné que le secteur de l'aide à domicile est en perpétuel essor, les emplois dans ce domaine ne manquent pas.</p>
       <div className="bloc-dates">
-        <div>
+        <div className="card-prochaine">
           <div>
-            <h3>Session ADVF complète</h3>
-            <p>11 janvier 2022 au 20 Mai 2022</p>
+            <h3>Session Majordomat complète</h3>
+            <p>1 avril 2022 au 21 avril 2022</p>
           </div>
           <div className="preinscription">
             <Button label="Me préinscrire" onClick={() => router.push("/prochaines-sessions#contact")} />
           </div>
-        </div>
-        <div>
+        </div>    
+        <div className="card-prochaine">
           <div>
-            <h3>Session CCP1</h3>
-            <p>11 janvier 2022 au 23 février 2022</p>
+            <h3>Module Etiquette complète</h3>
+            <p>1 avril 2022 au 5 avril 2022</p>
           </div>
           <div className="preinscription">
             <Button label="Me préinscrire" onClick={() => router.push("/prochaines-sessions#contact")} />
           </div>
-        </div>
-        <div>
+        </div>    
+        <div className="card-prochaine">
           <div>
-            <h3>Session CCP2</h3>
-            <p>28 février 2022 au 08 avril 2022</p>
+            <h3>Module Art de la table complète</h3>
+            <p>6 avril 2022 au 8 avril 2022</p>
           </div>
           <div className="preinscription">
             <Button label="Me préinscrire" onClick={() => router.push("/prochaines-sessions#contact")} />
           </div>
-        </div>
-        <div>
+        </div>    
+        <div className="card-prochaine">
+          <div>
+            <h3>Module Oenologie (Science et connaissance des vins) complète</h3>
+            <p>11 avril 2022 au 12 avril 2022</p>
+          </div>
+          <div className="preinscription">
+            <Button label="Me préinscrire" onClick={() => router.push("/prochaines-sessions#contact")} />
+          </div>
+        </div>    
+        <div className="card-prochaine">
+          <div>
+            <h3>Module Gastronomie complète</h3>
+            <p>14 avril 2022 au 21 avril 2022</p>
+          </div>
+          <div className="preinscription">
+            <Button label="Me préinscrire" onClick={() => router.push("/prochaines-sessions#contact")} />
+          </div>
+        </div>    
+        <div className="card-prochaine">
           <div>
             <h3>Session CCP3</h3>
             <p>11 avril 2022 au 20 mai 2022</p>
--- components/ProchainesSessions/InformationsCollectives.tsx
+++ components/ProchainesSessions/InformationsCollectives.tsx
@@ -9,19 +9,8 @@ const InformationsCollectives = () => {
       <h2>Information collective du vendredi</h2>
       <hr />
       <p>Chaque vendredi à 11H, nous vous accueillons dans nos locaux ou en visio pour répondre à vos questions et nous présenter.</p>
-
-      <Suspense fallback={<ProgressSpinner />}>
-        <br />
-        <br />
-        <Image
-          loader={myLoader}
-          alt="calendrier avril 2022"
-          src="/images/calendrier/sap_calendrier_avril_2022.png"
-          quality={80}
-          height={629}
-          width={919}
-        />
-      </Suspense>
+      <p>Vous n'avez pas les moyens de vous former ? Nous avons une solution pour vous ! Si vous êtes demandeur d'emploi, étudiant en alternance, salarié, chef d'entreprise... Vous pouvez bénéficier d'une formation finançable.</p>
+      <p>Faire une formation, c'est investir dans l'avenir. C'est pour cela que la plupart des formations sont éligibles au CPF et en partie à la rémunération.</p>
     </section>
   );
 };
