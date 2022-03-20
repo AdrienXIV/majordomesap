ac9ec5bc9232ae2a39cf816eb5ec7661f7e1b242
Adrien M <adrien.mld@laposte.net>
2022-03-20 16:39:35 +0100

    🐛 FIX:

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,42 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <meta name="robots" content="all" />
+        <meta name="google-site-verification" content="-FrNyJNyESokbOqGvXDz0oIWwgDnQ05eNb-D1Z1R5CM" />
+        <meta name="Content-Type" content="UTF-8" />
+        <meta name="Content-Language" content="fr" />
+        <meta name="Subject" content="Association de formation" />
+        <meta name="Copyright" content="Dev Agency" />
+        <meta name="Author" content="Allan BUSI, Adrien Maillard, Sarah Ouriachi" />
+        <meta name="Publisher" content="Allan BUSI" />
+        <meta name="Identifier-Url" content="https://majordomeformationsap.com/" />
+        <meta name="Revisit-After" content="31 days" />
+        <meta name="Rating" content="general" />
+        <meta name="Distribution" content="global" />
+        <meta name="Expires" content="12/12/2025" />
+        <meta name="Geography" content="Issy les Moulinaux" />
+        <meta name="Category" content="economics" />
+        <meta name="DC.Title" content="Sap Majordome" />
+        <meta name="DC.Content-Type" content="UTF-8" />
+        <meta name="DC.Content-Language" content="fr" />
+        <meta name="DC.Description" content="Formation majordome et d'assistance de vie" />
+        <meta name="DC.Keywords" content="Formation, Assitance, Majordome, Vie" />
+        <meta name="DC.Subject" content="Association de formation" />
+        <meta name="DC.Copyright" content="Dev Agency" />
+        <meta name="DC.Author" content="Allan BUSI, Adrien Maillard, Sarah Ouriachi" />
+        <meta name="DC.Publisher" content="Allan BUSI" />
+        <meta name="DC.Identifier-Url" content="https://majordomeformationsap.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="twitter:title" content="Sap Formation" />
+        <meta property="twitter:description" content="A remplir" />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
