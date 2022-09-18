62941bc075049cc3a547a1b50d5886e5bd79c2b5
Adrien M <adrien.mld@laposte.net>
2022-09-18 18:15:38 +0200

    👌 IMPROVE: suppression dates

--- pages/prochaines-sessions.tsx
+++ pages/prochaines-sessions.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
 import Header from "@components/other/Header";
-import Head from "next/head";
-import DatesSessions from "@components/ProchainesSessions/DatesSessions";
 import InformationsCollectives from "@components/ProchainesSessions/InformationsCollectives";
 
 const ProchainesSessionsPage: NextPage = () => {
@@ -20,7 +18,7 @@ const ProchainesSessionsPage: NextPage = () => {
 
       <Header />
       <InformationsCollectives />
-      <DatesSessions />
+      {/* <DatesSessions /> */}
     </main>
   );
 };
